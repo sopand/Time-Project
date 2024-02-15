fb7dbaac68439d39ecddbc1840bcfa163424b738
sopand <pows1031@naver.com>
2024-02-16 00:25:04 +0900

    time first

---
+++ src/main/java/com/time/TimeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/time/TimeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
